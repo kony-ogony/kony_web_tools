580b40f67c48b9e0a344f958348ae0e9996cb9a8
PadowYT2 <me@padow.ru>
2024-08-03 22:28:57 +0300

    add padow

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import Fastify from 'fastify';
 import bearerAuthPlugin from '@fastify/bearer-auth';
 import ngrok from '@ngrok/ngrok';
 import { exec } from 'node:child_process';
+import { $ } from 'bun';
 
 const fastify = Fastify();
 fastify.register(bearerAuthPlugin, { keys: [process.env['BEARER_TOKEN']!] });
@@ -17,8 +18,10 @@ fastify.post('/shutdown', async (request, _) => {
 });
 
 await fastify.listen({ port: 3000 });
+
+const who = await $`hostname`.text();
 await ngrok.forward({
-    domain: 'quietly-nice-bull.ngrok-free.app',
+    domain: `${who === 'kony' ? 'quietly-nice-bull' : 'arriving-fairly-dodo'}.ngrok-free.app`,
     addr: 3000,
-    authtoken: process.env['NGROK_AUTHTOKEN'],
+    authtoken: process.env[`${who.toUpperCase()}_AUTHTOKEN`],
 });
